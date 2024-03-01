6bf1a3dc1f896e1453d3a38ee42c348940876ef6
Baek <woonn97@gmail.com>
2024-03-01 13:24:54 +0900

    style: 전체 스타일링 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import List from "./components/List";
 import NewTaskForm from "./components/NewTaskForm";
+import { AppWrapper, Header } from "./style";
 
 function App() {
   return (
-    <div>
+    <AppWrapper>
+      <Header>To Do List</Header>
       <NewTaskForm />
       <List />
-    </div>
+    </AppWrapper>
   );
 }
 
